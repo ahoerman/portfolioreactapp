44b2ff52de47e408eea6e437db3c7f3eefa7e48a
A Hoerman <awhoerman@me.com>
2020-05-17 22:04:38 -0400

    Updates

---
+++ precache-manifest.b30491fc146522fbbc54f644c93c0786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba10b3fc112200b1191bb3ffa9e17412",
+    "url": "/portfolioreactapp/index.html"
+  },
+  {
+    "revision": "4ce52bd8e54fd5ee2664",
+    "url": "/portfolioreactapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de6c2b9b015b285b94de",
+    "url": "/portfolioreactapp/static/js/2.91c5a00a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolioreactapp/static/js/2.91c5a00a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce52bd8e54fd5ee2664",
+    "url": "/portfolioreactapp/static/js/main.ad441aaf.chunk.js"
+  },
+  {
+    "revision": "cc0148072739cf376be5",
+    "url": "/portfolioreactapp/static/js/runtime-main.7f74b6fb.js"
+  }
+]);
